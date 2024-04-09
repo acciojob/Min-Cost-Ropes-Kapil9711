0fe470bf668ea4437d3bd5f45f3cd2ce27888648
majestice_coder <142192726+Kapil9711@users.noreply.github.com>
2024-04-09 23:16:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function mincost(arr)
 	let tempArr = [...arr];
 	let res = 0;
 	tempArr.sort((a,b)=>a-b);
-	while(temp.length > 1){
+	while(tempArr.length > 1){
 		const n1 = tempArr.shift();
 		const n2 = tempArr.shift();
 		res += n1 + n2
